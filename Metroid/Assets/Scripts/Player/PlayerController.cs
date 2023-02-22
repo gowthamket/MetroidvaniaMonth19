f2ef1ccfae2c0add2d37ce520c09d6012af62e00
gowthamket <65178336+gowthamket@users.noreply.github.com>
2023-02-21 21:29:37 -0500

    PlayerController additions

--- Metroid/Assets/Scripts/Player/PlayerController.cs
+++ Metroid/Assets/Scripts/Player/PlayerController.cs
@@ -7,25 +7,66 @@ public class PlayerController : MonoBehaviour
     private float movementInputDirection;
     private Rigidbody2D rb;
 
+    private int amountOfJumpsLeft;
+
     private bool isFacingRight = true;
+    private bool isGrounded;
+    private bool canJump;
+
+    public int amountOfJumps = 1;
 
     public float movementSpeed = 10.0f;
     public float jumpForce = 16.0f;
+    public float groundCheckRadius;
+    private bool isTouchingWall;
+    public float wallCheckDistance;
+
+    public LayerMask whatIsGround;
+
+    public Transform groundCheck;
+    public Transform wallCheck;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     private void Update()
     {
         CheckInput();
         CheckMovementDirection();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+    }
+
+    private void CheckIfCanJump()
+    {
+        if (isGrounded && rb.velocity.y <= 0)
+        {
+            amountOfJumpsLeft = amountOfJumps;
+        }
+
+        if (amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
     }
 
     private void CheckMovementDirection()
@@ -52,7 +93,11 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        
     }
 
     private void ApplyMovement()
@@ -64,5 +109,12 @@ public class PlayerController : MonoBehaviour
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y, wallCheck.position.z));
     }
 }
