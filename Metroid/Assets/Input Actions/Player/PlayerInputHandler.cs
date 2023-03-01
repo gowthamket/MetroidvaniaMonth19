d83c098d4d20838bdbf5f5493a53ff6c08c91d9c
gowthamket <65178336+gowthamket@users.noreply.github.com>
2023-03-01 11:29:40 -0500

    Input Action System

---
+++ Metroid/Assets/Input Actions/Player/PlayerInputHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Vector2 movementInput;
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+
+        }
+    }
+}
